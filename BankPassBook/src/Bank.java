8013817a994d0570142013920db8e7c3c7b2978e
Shalitha Jayamal <47962458+ShalithaJayamal@users.noreply.github.com>
2019-12-31 12:25:20 +0530

    Add files via upload

--- BankPassBook/src/Bank.java
+++ BankPassBook/src/Bank.java
@@ -1,50 +1,45 @@
 import java.util.Scanner;
 
-public class Bank {
-	
+class Bank
+{
 	private String accno;
 	private String name;
 	private long balance;
+
+	Scanner KB=new Scanner(System.in);
 	
-	Scanner KB  = new Scanner(System.in);
-	
-	void openAccount ()
-	{
-		
-		System.out.println("Enter the Account Name ");
-		accno = KB.next();
-		
-		
+	//method to open an account
+	void openAccount()
+	{ 
+		System.out.print("Enter Account No: ");
+		accno=KB.next();
 		System.out.print("Enter Name: ");
 		name=KB.next();
-		
-		
 		System.out.print("Enter Balance: ");
 		balance=KB.nextLong();
-	
-		
-		
 	}
-	
+
+	//method to display account details
 	void showAccount()
-	{System.out.println(accno +" "+name +" " +balance);}
-	
-	
+	{ 
+		System.out.println(accno+","+name+","+balance);
+	}
+
+	//method to deposit money
 	void deposit()
 	{
 		long amt;
-		System.out.println("Enter you want  to deposit Ammount");
-		amt = KB.nextLong();
-		balance = balance + amt;
+		System.out.println("Enter Amount U Want to Deposit : ");
+		amt=KB.nextLong();
+		balance=balance+amt;
 	}
-	
+
+	//method to withdraw money
 	void withdrawal()
 	{
-		long amt ;
-		System.out.println("Enter your ammount  to withdrawal");
-		amt  = KB.nextLong();
-		
-		
+		long amt;
+		System.out.println("Enter Amount U Want to withdraw : ");
+		amt=KB.nextLong();
 		if(balance>=amt)
 		{ 
 			balance=balance-amt;
@@ -53,23 +48,16 @@ public class Bank {
 		{
 			System.out.println("Less Balance..Transaction Failed..");
 		}
-		
-		
 	}
-	
+
+	//method to search an account number
 	boolean search(String acn)
-	{
+	{ 
 		if(accno.equals(acn))
-		{
+		{ 
 			showAccount();
 			return(true);
 		}
-		
-		
-		return (false);
+		return(false);
 	}
-	
-	
-	
-
 }
--- BankPassBook/src/ExBank.java
+++ BankPassBook/src/ExBank.java
@@ -1,31 +1,99 @@
 import java.util.Scanner;
 
+
 class ExBank
-{
-	public static void main(String arg [])
+{  
+	public static void main(String arg[])
 	{
 		Scanner KB=new Scanner(System.in);
 		
+		//create initial accounts
 		System.out.print("How Many Customer U Want to Input : ");
 		int n=KB.nextInt();
+		Bank  C[]=new Bank[n];
+		for(int i=0;i<C.length;i++)
+		{   
+			C[i]=new Bank();
+			C[i].openAccount();
+		}
 		
-		
-		
-		
-		
-		
-		
+		//run loop until menu 5 is not pressed
+		int ch;
+		do
+		{
+			System.out.println("Main Menu\n1.Display All\n2.Search By Account\n3.Deposit\n4.Withdrawal\n5.Exit");
+			System.out.println("Ur Choice :");
+			ch=KB.nextInt();
+			switch(ch)
+			{ 
+				case 1:
+					for(int i=0;i<C.length;i++)
+					{
+						C[i].showAccount();
+					}
+					break;
+
+				case 2:
+					System.out.print("Enter Account No U Want to Search...: ");
+					String acn=KB.next();
+					boolean found=false;
+					for(int i=0;i<C.length;i++)
+					{  
+						found=C[i].search(acn);
+						if(found)
+						{
+							break;
+						}
+					}
+					if(!found)
+					{
+						System.out.println("Search Failed..Account Not Exist..");
+					}
+					break;
+
+				case 3:
+					System.out.print("Enter Account No : ");
+					acn=KB.next();
+					found=false;
+					for(int i=0;i<C.length;i++)
+					{  
+						found=C[i].search(acn);
+						if(found)
+						{
+							C[i].deposit();
+							break;
+						}
+					}
+					if(!found)
+					{
+						System.out.println("Search Failed..Account Not Exist..");
+					}
+					break;
+
+				case 4:
+					System.out.print("Enter Account No : ");
+					acn=KB.next();
+					found=false;
+					for(int i=0;i<C.length;i++)
+					{  
+						found=C[i].search(acn);
+						if(found)
+						{
+							C[i].withdrawal();
+							break;
+						}
+					}
+					if(!found)
+					{
+						System.out.println("Search Failed..Account Not Exist..");
+					}
+					break;
+
+				case 5:
+					System.out.println("Good Bye..");
+					break;
+			}
+		}
+		while(ch!=5);
 	}
-	
-
-
-
-
-
-
-
-
-
-
-
 }
