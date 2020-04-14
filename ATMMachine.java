eb499abcebc3b0ed36e4baf62e1c9492bd6bfcbc
Shalitha Jayamal <47962458+ShalithaJayamal@users.noreply.github.com>
2020-04-14 23:52:22 +0530

    Add files via upload

---
+++ ATMMachine.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ATMMachine {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String userName = "Shalitha  jayamal";
+		String password = "password";
+		String bankName = "Amana Bank";
+		double useBalance = 100000;
+		
+		Scanner bankScanner = new Scanner(System.in);
+		
+		System.out.println("Welcome to "+bankName);
+		
+		System.out.println("Please Enter your bank Acc Number");
+	}
+
+}
